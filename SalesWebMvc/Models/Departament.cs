9a28f13bc6ad4d1bdeeced0ce0ac72ba02dc2792
YuriMacedo <yuri_paulino@hotmail.com>
2022-07-28 09:55:53 -0300

    Adicionar arquivos de projeto.

---
+++ SalesWebMvc/Models/Departament.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace SalesWebMvc.Models
+{
+    public class Departament
+    {
+        public int id { get; set; }
+        public string  name { get; set; }
+        public ICollection<Seller> Sellers { get; set; } = new List<Seller>();
+
+
+        public Departament(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+        }
+
+        public Departament()
+        {
+        }
+
+        public void addSeller(Seller seller) {
+            Sellers.Add(seller);
+        }
+
+        public double TotalSales(DateTime initial, DateTime final) {
+            return Sellers.Sum(seller => seller.totalSales(initial, final));
+        }
+    }
+}
