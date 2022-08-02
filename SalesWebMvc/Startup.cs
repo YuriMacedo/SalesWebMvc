0e0a3cb970a042a473d768b431aa65a1ad5b334b
YuriMacedo <yuri_paulino@hotmail.com>
2022-08-02 09:29:38 -0300

    Localization, Numbers and Date format

--- SalesWebMvc/Startup.cs
+++ SalesWebMvc/Startup.cs
@@ -12,6 +12,9 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using SalesWebMvc.Data;
 using SalesWebMvc.Services;
+using System.Globalization;
+using Microsoft.AspNetCore.Localization;
+
 
 
 namespace SalesWebMvc
@@ -53,6 +56,13 @@ namespace SalesWebMvc
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env,SeedingService seedingService)
         {
+
+            var enUS = new CultureInfo("en-US");
+            var localizations = new RequestLocalizationOptions { DefaultRequestCulture = new RequestCulture(enUS),
+            SupportedCultures = new List<CultureInfo> { enUS }, SupportedUICultures = new List<CultureInfo> { enUS }
+            };
+
+            app.UseRequestLocalization(localizations);
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
